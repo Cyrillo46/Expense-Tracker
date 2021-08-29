1885a582868188e2bb3408040a9f0822b3a6c1ce
Ant <yahooisbetter46@gmail.com>
2021-08-29 19:57:36 -0400

    Added multiple items to component

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>August 28th, 2021</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">232.19</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
